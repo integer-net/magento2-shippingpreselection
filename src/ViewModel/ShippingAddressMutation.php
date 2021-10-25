2b8e208fe36d359ee8fd677ca3147bcb83538418
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2021-10-25 15:42:05 +0530

    #5 Remove Hyva dependency: \Hyva\Theme\ViewModel\Cart\GraphQlQueries from the module

--- src/ViewModel/ShippingAddressMutation.php
+++ src/ViewModel/ShippingAddressMutation.php
@@ -3,45 +3,44 @@ declare(strict_types=1);
 
 namespace IntegerNet\ShippingPreselection\ViewModel;
 
+use IntegerNet\ShippingPreselection\ViewModel\Cart\GraphQlQueries;
 use Magento\Directory\Model\Country;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-use Hyva\Theme\ViewModel\Cart\GraphQlQueries;
 
 class ShippingAddressMutation implements ArgumentInterface
 {
     private GraphQlQueries $graphQlQueries;
 
-    public function __construct(
-        GraphQlQueries $graphQlQueries)
+    public function __construct(GraphQlQueries $graphQlQueries)
     {
         $this->graphQlQueries = $graphQlQueries;
     }
 
     public function getMutateAddressQuery()
     {
-        return 'setShippingAddressesOnCart(
-                        input: {
-                        cart_id: "${this.cartId}"
-                        shipping_addresses: [
-                            {
-                                address: {
-                                    firstname: "${addressDataFromCart.firstname}"
-                                    lastname: "${addressDataFromCart.lastname}"
-                                    street: "${addressDataFromCart.street}"
-                                    city: "${addressDataFromCart.city}"
-                                    region: "${addressDataFromCart.region.code}"
-                                    country_code: "${countryCode}"
-                                    telephone: "${addressDataFromCart.telephone}"
-                                    postcode: "${addressDataFromCart.postcode}"
-                                    save_in_address_book: false
-                                }
-                            }
-                        ]
-                    }
-                ){
+        return '
+            setShippingAddressesOnCart(
+                input: {
+                    cart_id: "${this.cartId}"
+                    shipping_addresses: [{
+                        address: {
+                            firstname: "${addressDataFromCart.firstname}"
+                            lastname: "${addressDataFromCart.lastname}"
+                            street: "${addressDataFromCart.street}"
+                            city: "${addressDataFromCart.city}"
+                            region: "${addressDataFromCart.region.code}"
+                            country_code: "${countryCode}"
+                            telephone: "${addressDataFromCart.telephone}"
+                            postcode: "${addressDataFromCart.postcode}"
+                            save_in_address_book: false
+                        }
+                    }]
+                }
+            ) {
                cart {
                   ' . $this->graphQlQueries->getCartGraphQlQuery() . '
                 }
-            }';
+            }
+        ';
     }
 }
